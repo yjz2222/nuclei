102147e2c6377f87018a724937080d1212a5df7a
徐兆康 <jaz@vackbot.com>
2022-06-08 16:21:15 +0800

    fix:初始化用户名密码语句优化

--- v2/pkg/rest/db/migrate.go
+++ v2/pkg/rest/db/migrate.go
@@ -110,8 +110,8 @@ CREATE TABLE IF NOT EXISTS "public".users (
 	CONSTRAINT unq_name UNIQUE ( user_name ) 
  );
 
-INSERT INTO public.users (user_name, password) select 'admin', '123456' from public.users where not exists 
-(select * from public.users where user_name='admin');
+INSERT INTO  public.users (user_name, password) VALUES ('admin', '123456') ON CONFLICT (user_name) DO NOTHING;
+
 
 `
 
